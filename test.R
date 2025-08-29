8332404ee72311ef110b9d782d140acc97fa7a30
bjovic-git <branka.jovic@phac-aspc.gc.ca>
2025-08-29 13:53:26 +0000

    Commit 'red' plot

--- test.R
+++ test.R
@@ -1,2 +1,8 @@
 #new file
-library(janitor)
+# Create data
+x <- c(1, 2, 3, 4, 5)
+y <- c(2, 4, 6, 8, 10)
+
+# Plot the data
+plot(x, y, type = "o", col = "red", main = "Simple Line Plot", xlab = "X-axis", ylab = "Y-axis")
+
