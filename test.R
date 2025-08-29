fee142f9fb0f6a4bd4f2e03357f245ca2be8e1ff
bjovic-git <branka.jovic@phac-aspc.gc.ca>
2025-08-29 13:57:25 +0000

    Commit plot with red line

--- test.R
+++ test.R
@@ -4,5 +4,5 @@ x <- c(1, 2, 3, 4, 5)
 y <- c(2, 4, 6, 8, 10)
 
 # Plot the data
-plot(x, y, type = "o", col = "blue", main = "Simple Line Plot", xlab = "X-axis", ylab = "Y-axis")
+plot(x, y, type = "o", col = "red", main = "Simple Line Plot", xlab = "X-axis", ylab = "Y-axis")
 
